d3e397de1ae9678341a2538af3110c642ad80f7a
Michael Belov <mikle0706@icloud.com>
2021-12-07 12:53:27 +0300

    Добавил контент для завершения счетчика

--- src/App.js
+++ src/App.js
@@ -1,58 +1,105 @@
 import styled from "styled-components";
 import React from "react";
-import Snowflakes from 'magic-snowflakes';
-import './App.css';
+import Snowflakes from "magic-snowflakes";
+import Bars from "./bars.jpg";
+import Star from "./Pixi/Star";
+import "./App.css";
 
-const ValueBlock = React.memo(({value, title}) => (
- <TimeBlock ><div>{value}</div><div>{title}</div></TimeBlock>
+const ValueBlock = React.memo(({ value, title }) => (
+  <TimeBlock>
+    <div>{value}</div>
+    <div>{title}</div>
+  </TimeBlock>
 ));
 
 let interval;
+const windowInnerWidth = window.innerWidth;
+const windowInnerHeight = window.innerHeight;
 
 function App() {
-    const [date, setDate] = React.useState({});
+  const [date, setDate] = React.useState({});
+  const [happyEnd, setHappyEnd] = React.useState("");
 
-    React.useEffect(() => {
-        const snowflakes = new Snowflakes({ count: 100, speed: 1.5 });
-        snowflakes.start();
+  React.useEffect(() => {
+    if (happyEnd === "stars") {
+      document.getElementById("star").appendChild(Star.view);
+      setTimeout(() => {
+        document.querySelector("canvas").remove();
+        setHappyEnd("video");
+      }, 10000);
+    }
+  }, [happyEnd]);
 
-        interval = setInterval(() => {
-          const currentDay = new Date();
-          const finallyDay = new Date("2021-12-10T18:00:00");
-          const msDate = finallyDay - currentDay;
-          const valueDay = Math.floor(msDate / (1000 * 60 * 60 * 24));
-          const valueHours = Math.floor((msDate / (1000 * 60 * 60) % 24));
-          const valueMinute = Math.floor((msDate / (1000 * 60)) % 60);
-          const valueSec = Math.floor((msDate / 1000) % 60);
-          const valueMs = Math.floor(msDate % 1000);
-  
-          setDate({valueDay, valueHours, valueMinute, valueSec, valueMs});
-        }, 250)
+  React.useEffect(() => {
+    if (date.valueSec < 0 && !happyEnd) {
+      setHappyEnd("stars");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [date]);
 
-        return () => {
-            snowflakes.destroy();
-            clearInterval(interval);
-        }
-    }, []);
+  React.useEffect(() => {
+    const snowflakes = new Snowflakes({ count: 100, speed: 1.5 });
 
+    interval = setInterval(() => {
+      const currentDay = new Date();
+      const finallyDay = new Date("2021-12-10T18:00:00");
+      const msDate = finallyDay - currentDay;
+      const valueDay = Math.floor(msDate / (1000 * 60 * 60 * 24));
+      const valueHours = Math.floor((msDate / (1000 * 60 * 60)) % 24);
+      const valueMinute = Math.floor((msDate / (1000 * 60)) % 60);
+      const valueSec = Math.floor((msDate / 1000) % 60);
+      const valueMs = Math.floor(msDate % 1000);
 
+      setDate({ valueDay, valueHours, valueMinute, valueSec, valueMs });
+    }, 250);
 
-    return (
+    return () => {
+      snowflakes.destroy();
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
     <PageWrapper className="App">
-        <h4>Осталось</h4>
-        <TimeWrapp>
-            <ValueBlock value={date.valueDay} title="Days" />
-            <ValueBlock value={date.valueHours} title="Hours" />
-            <ValueBlock value={date.valueMinute} title="Minutes" />
-            <ValueBlock value={date.valueSec} title="Seconds" />
-            <ValueBlock value={date.valueMs} title="Ms" />
-        </TimeWrapp>
+      <div id="star">
+        {happyEnd === "video" && (
+          <>
+            <button
+              onClick={() => {
+                setHappyEnd("end");
+              }}
+              className="button"
+            >
+              X
+            </button>
+            <iframe
+              id="video"
+              width={windowInnerWidth}
+              height={windowInnerHeight}
+              src="https://www.youtube.com/embed/Gm8UYntIb0o?autoplay=1&mute=1"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            />
+          </>
+        )}
+      </div>
+
+      <h4>Осталось</h4>
+      <TimeWrapp>
+        <ValueBlock value={date.valueDay} title="Days" />
+        <ValueBlock value={date.valueHours} title="Hours" />
+        <ValueBlock value={date.valueMinute} title="Minutes" />
+        <ValueBlock value={date.valueSec} title="Seconds" />
+        <ValueBlock value={date.valueMs} title="Ms" />
+      </TimeWrapp>
+      {date.valueSec < 0 && <img src={Bars} alt="bars" />}
     </PageWrapper>
   );
 }
 
 export default App;
-
 
 const PageWrapper = styled.div`
   min-height: calc(100vh - 100px);
@@ -65,9 +112,26 @@ const PageWrapper = styled.div`
 
   background-color: #d3fdfd;
   padding: 50px;
-  
+
   h4 {
     margin: 30px;
+  }
+
+  img {
+    margin-top: 20px;
+  }
+
+  .button {
+    position: absolute;
+    top: 0;
+    right: 0;
+    background-color: red;
+    width: 50px;
+    height: 50px;
+  }
+
+  #star {
+    position: fixed;
   }
 `;
 
